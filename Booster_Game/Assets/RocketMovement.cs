f2117d715b17394552c5c84055c9474d62d79174
Unknown <koen_jolle@hotmail.com>
2019-01-15 12:53:17 +0100

    Basic Input Binding

---
+++ Booster_Game/Assets/RocketMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       processInput();
+    }
+
+    private void processInput()
+    {
+        //Thrust while rotating
+        if(Input.GetKey(KeyCode.Space)){
+            print("spaaace");
+        }
+
+        //Rotating either left or right
+        if(Input.GetKey(KeyCode.A)){
+            print("leeeeft");
+        } else if (Input.GetKey(KeyCode.D)){
+            print("riiiigth");
+        }
+    }
+}
