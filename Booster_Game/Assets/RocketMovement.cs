40c2e2b43784f60195c9335420261c198a758588
Unknown <koen_jolle@hotmail.com>
2019-01-17 13:30:25 +0100

    Invoke() As A Coroutine Warm-Up

--- Booster_Game/Assets/RocketMovement.cs
+++ Booster_Game/Assets/RocketMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class RocketMovement : MonoBehaviour
 {
-
+    //SerializeField make it so you can adjust the value in the inspector in Unity
     [SerializeField] float rcsThrust = 300f;
     [SerializeField] float mainThrust = 10f;
 
@@ -14,11 +14,13 @@ public class RocketMovement : MonoBehaviour
     AudioSource audioSource;
 
     enum State {Alive,Dead,Transcending}
+    [SerializeField] State state = State.Alive;
 
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        //Gets the components from game object in Unity
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
@@ -26,71 +28,95 @@ public class RocketMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Thrust();
-       Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate(); 
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+
+
+
+        //Handles collision of the rocket based on the tags of the objects it bumps into
         switch(collision.gameObject.tag){
 
             case "Friendly":
-                print("U alive boi");
+                print("U alive boi"); //Remove
                 break;
             case "Fuel":
-                print("Fuel regain yeah");
+                print("Fuel regain yeah"); //Todo maybe remove
                 break;
             case "Finish":
-                SceneManager.LoadScene(1);
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
-                SceneManager.LoadScene(0);
+                state = State.Dead;
+                Invoke("HandlePlayerDeath", 1.5f);
                 break;
         }        
     }
 
+    private void HandlePlayerDeath()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //Make other levels available. Make currentlevel prob.
+    }
+
     private void Thrust()
     {
-
         //Thrust while rotating
-        if (Input.GetKey(KeyCode.W)){
+        if (Input.GetKey(KeyCode.W))
+        {
             rigidbody.AddRelativeForce(Vector3.up * mainThrust);
-            playRocketSound();
-        } else if (Input.GetKey(KeyCode.S)){
-            rigidbody.AddRelativeForce(Vector3.down * mainThrust);
-            playRocketSound();
-        } else{
-            stopRocketSound();
+            PlayRocketSound();
         }
-
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddRelativeForce(Vector3.down * mainThrust);
+            PlayRocketSound();
+        }
+        else
+        {
+            StopRocketSound();
+        }
     }
 
     private void Rotate()
-    {
-        
+    {       
         rigidbody.freezeRotation = true; // Take manual control of rotation
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         //Rotating either left or right
-        if (Input.GetKey(KeyCode.A)){
+        
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-        else if (Input.GetKey(KeyCode.D)){
+        else if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
-        }
-
+        } 
+        
         rigidbody.freezeRotation = false; // Resume physics control of rotation
     }
 
-    private void stopRocketSound()
+    private void StopRocketSound()
     {
         if(audioSource.isPlaying){
             audioSource.Stop();
         }
     }
 
-    private void playRocketSound(){
+    private void PlayRocketSound(){
         if(!audioSource.isPlaying){
             audioSource.Play();
         }
